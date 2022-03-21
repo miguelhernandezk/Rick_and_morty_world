01eb2ae310e12b7a776bdd0fce9fff24e52a6fea
Miguel Hernández <miguelhernandezk@gmail.com>
2022-03-21 13:02:24 -0600

    Se cambió la api de aguacates por la de Rick y Morty

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const baseUrl =  "https://rickandmortyapi.com/api/character"
+const appNode = document.querySelector("#app")
 
-console.log('Happy hacking :)')
+const fetchData = async (url) => {
+    const allItems = [];
+    try{
+        const response =  await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        data.results.forEach(item => {
+            // Create image
+            const image = document.createElement("img");
+            image.src = item.image;
+
+
+            // Create name
+            const name = document.createElement("h2");
+            name.textContent = item.name;
+
+
+            // Create species
+            const species = document.createElement("div");
+            species.textContent = item.species;
+            
+            const container = document.createElement("div");
+            container.append(image, name, species);
+            container.className = "rounded-xl bg-gray-700"
+
+            allItems.push(container); 
+        });
+        appNode.className = "grid grid-cols-3";
+        appNode.append(...allItems);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+fetchData(baseUrl);
